918d6701cfb7a042a5613d6aaa9dec9b2e2210df
pranabesh11 <pranabeshpratiharjgm@gmail.com>
2024-05-03 15:46:35 +0530

    some small features like delete and mark suspecious

--- app/controllers/expense_controller.rb
+++ app/controllers/expense_controller.rb
@@ -62,6 +62,8 @@ class ExpenseController < ApplicationController
         split_bill_from_a_person_to = SplitABill.where(borrower:current_user.id.to_s)
         @bill_to_a_person = tag_bill_from_a_person_to + split_bill_from_a_person_to
 
+        # this code is to get all the data 
+
         # code to add payment status
         p "=========all parama passing wihle clicking the payemnt done btn ==========="
         p params[:lender]
@@ -95,6 +97,9 @@ class ExpenseController < ApplicationController
         else
             p "Something Happened I don't know anything about it"
         end
+
+
+
         p "==========*****=============params for delete request============****=============="
         p params[:lender]
         p params[:borrower]
@@ -106,9 +111,9 @@ class ExpenseController < ApplicationController
             present_in_tag_a_bill_table = TagABill.find_by(lender: params[:lender], unique_expense_id: params[:unique_expense_id], borrower:current_user.id)
             present_in_split_a_bill_table = SplitABill.find_by(lender: params[:lender], unique_expense_id: params[:unique_expense_id] , borrower:current_user.id)
             if present_in_tag_a_bill_table
-                present_in_tag_a_bill_table.update(delete_status: "Deleted")
+                present_in_tag_a_bill_table.update(delete_status: "Suspicious")
             elsif present_in_split_a_bill_table
-                present_in_split_a_bill_table.update(delete_status: "Deleted")
+                present_in_split_a_bill_table.update(delete_status: "Suspicious")
             else
                 p "Expense not found in any table."
             end
@@ -117,9 +122,9 @@ class ExpenseController < ApplicationController
             present_in_tag_a_bill_table = TagABill.find_by(borrower: params[:borrower], unique_expense_id: params[:unique_expense_id], lender:current_user.id)
             present_in_split_a_bill_table = SplitABill.find_by(borrower: params[:borrower], unique_expense_id: params[:unique_expense_id] , lender:current_user.id)
             if present_in_tag_a_bill_table
-                present_in_tag_a_bill_table.update(delete_status: "Delete_request")
+                present_in_tag_a_bill_table.delete
             elsif present_in_split_a_bill_table
-                present_in_split_a_bill_table.update(delete_status: "Delete_request")
+                present_in_split_a_bill_table.delete
             else
                 p "Expense not found in any table."
             end
